c9a12e011dd8139448468405ee8f768d1cad445d
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-04-03 02:14:58 +0200

    home section and add media query

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// scroll down affects navbar
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
