73826d9d26401d9ca60b7635116d67e88febbad6
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-05-07 03:37:04 +0200

    typing text animation

--- js/main.js
+++ js/main.js
@@ -13,12 +13,25 @@ $(document).ready(function() {
         }
     });
     // slide-up script
-    $('scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function(){
+        $("html").animate({ scrollTop: 0 });
     });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Blogger", "Freelancer", "Youtuber", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Blogger", "Freelancer", "Youtuber", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 });
